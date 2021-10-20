4671c9684c444b6dedf55782b966474f5cdd7b6e
Inara Oliveira <inaraolliveira23@hotmail.com>
2021-10-19 22:10:54 -0300

    Envia atividade 1 do dia 11/10

---
+++ src/Mensagem.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Mensagem extends Component {
+    render(){
+        return(
+            <div>
+                <h2>Sarah Westphal</h2>
+                <p>Desconfie do destino e acredite em você. Gaste mais horas realizando que sonhando, fazendo que
+                planejando, vivendo que esperando porque, embora quem quase morre esteja vivo, quem quase vive
+                já morreu.</p>
+            </div>
+        )
+    }
+}
+export default Mensagem;
